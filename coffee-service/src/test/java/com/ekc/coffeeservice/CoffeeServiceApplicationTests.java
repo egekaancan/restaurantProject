e730b7b3f85f6adb3d0f8cbd457ba8a68fe8a68b
egekaancan <egekcan9@gmail.com>
2023-04-22 18:18:12 +0300

    First Commit

---
+++ coffee-service/src/test/java/com/ekc/coffeeservice/CoffeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekc.coffeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
